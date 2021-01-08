1765e07ab8b7b07a4795cb1367df6276955e1554
marcus-sa <marcus-sa@outlook.com>
2021-01-08 14:45:11 +0100

    fix: release version stamping & missing brokers

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-set -ueo pipefail
+set -u -e -o pipefail
+
 # Call the script with argument "pack" or "publish"
 readonly NPM_COMMAND=${1:-publish}
-# Use a new output_base so we get a clean build
-# Bazel can't know if the git metadata changed
-readonly TMP=$(mktemp -d -t bazel-release.XXXXXXX)
-readonly BAZEL="bazel --output_base=$TMP"
+# Don't rely on $PATH to have the right version
+readonly BAZEL=./node_modules/.bin/bazel
 # Find all the npm packages in the repo
 readonly PKG_NPM_LABELS=`$BAZEL query --output=label 'kind("pkg_npm", //...)'`
 # Build them in one command to maximize parallelism
